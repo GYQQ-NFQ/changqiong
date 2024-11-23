6d7ef58f3372fd3f7cb4f8f58f9e9c2276e9b0d4
GYQQ <gyqq@gmail.com>
2024-11-23 18:20:10 +0800

    删除购物车商品业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -27,7 +28,21 @@ public interface ShoppingCartMapper {
      * 插入购物车数据
      * @param shoppingCart
      */
-    @Insert("insert into shopping_cart (name,user_id,setmeal_id,dish_flavor,number,image,create_time,amount)" +
-            "values (#{name},#{userId},#{setmealId},#{dishFlavor},#{number},#{image},#{createTime},#{amount})")
+    @Insert("insert into shopping_cart (id, name, image, user_id, dish_id, setmeal_id, dish_flavor,number, create_time, amount) " +
+            "values (#{id},#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{createTime},#{amount})")
     void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 根据传入userid清空购物车
+     * @param currentId
+     */
+    @Delete("delete from shopping_cart where user_id = #{currentId}")
+    void clean(Long currentId);
+
+    /**
+     * 根据id删除购物车商品
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/user/impl/ShoppingCartServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/user/impl/ShoppingCartServiceImpl.java
@@ -38,13 +38,15 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         //判断当前加入购物车的商品是否还存在
         ShoppingCart shoppingCart = new ShoppingCart();
         BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+
         Long userId = BaseContext.getCurrentId();
         shoppingCart.setUserId(userId);
 
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
 
         //如果已经存在了 只需要数量+1
-        if(list != null && !list.isEmpty()){
+        if(list != null && list.size() > 0){
+//            System.out.println("参考调试信息:"+list);
             ShoppingCart cart = list.get(0);
             cart.setNumber(cart.getNumber() + 1);
             shoppingCartMapper.updateNumberById(cart);
@@ -61,7 +63,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             }else {
                 //套餐
                 Long setmealId = shoppingCart.getSetmealId();
-
                 Setmeal setmeal = setmealMapper.getById(setmealId);
                 shoppingCart.setName(setmeal.getName());
                 shoppingCart.setImage(setmeal.getImage());
@@ -79,12 +80,47 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
      * @return
      */
     @Override
-    public List<ShoppingCart> showShopingCart() {
+    public List<ShoppingCart> showShoppingCart() {
         Long userId = BaseContext.getCurrentId();
         ShoppingCart shoppingCart = ShoppingCart.builder()
                 .userId(userId)
                 .build();
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         return list;
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @Override
+    public void cleanShoppignCart() {
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCartMapper.clean(currentId);
+    }
+
+    /**
+     * 删除商品
+     */
+    @Override
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        //当前userid
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        if (list != null && list.size() > 0){
+            //总是只获取第一个
+            shoppingCart = list.get(0);
+            Integer num = shoppingCart.getNumber();
+            if (num == 1){
+                shoppingCartMapper.deleteById(shoppingCart.getId());
+            }else {
+                shoppingCart.setNumber(shoppingCart.getNumber() - 1);
+                shoppingCartMapper.updateNumberById(shoppingCart);
+            }
+        }
     }
 }
